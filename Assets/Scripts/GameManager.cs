7ff328873ca00cf9ab6a426cce5f486c6c0ecf78
Matt Pinder <matt_pinder13@hotmail.com>
2025-03-04 10:02:08 +0100

    Move UI functionality to new script

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,33 +1,34 @@
 using System.Collections.Generic;
 using TMPro;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class GameManager : MonoBehaviour
 {
+    // Variables for letter spawning
     public GameObject[] letterPrefabs;                          // Array of letter prefabs (defined in Unity Editor)
-    public TextMeshProUGUI orbsRemainingText;                   // Text displaying the remaining orbs during gameplay
-    public TextMeshProUGUI lettersCompleteText;                 // Text displaying the number of completed letters during gameplay
-    public TextMeshProUGUI winText;                             // Text displaying when the player wins a round
-    public GameObject titleScreen;                              // GameObject containing title screen text
-    private PlayerTrail player;                                 // GameObject representing the player
-
-    public bool isGameActive = false;                           // Determine whether the game is active or not
-    public int orbsRemaining;                                   // How many orbs left to collect on the current letter
-    private int lettersComplete;                                // How many letters have been completed in the current session
-    public List<Vector3> letterShape = new List<Vector3>();     // List containing the Vector3 of all of a letter's orbs
-    // Find a way to convert letterShape to a property!
-
     public GameObject selectedLetter;                           // Letter defined by the button press
+    public int orbsRemaining;                                   // How many orbs left to collect on the current letter
+    public int lettersComplete { get; private set; }            // How many letters have been completed in the current session
+    // Find a way to convert letterShape to a property!
+    public List<Vector3> letterShape = new List<Vector3>();     // List containing the Vector3 of all of a letter's orbs
+    private float spawnDelay = 0.5f;                            // Seconds to wait before spawning a new letter
+    private int lettersToWin;                                   // Number of letters to complete to win    
+
+    // Other
+    public MenuUIController menuUIController;                   // Canvas's menu UI controller script
+    private PlayerTrail player;                                 // GameObject representing the player
+    public bool isGameActive = false;                           // Determine whether the game is active or not
     public string altGameMode = null;                           // Any alternate game mode selected (Upper/Lower/All)
 
-    private float spawnDelay = 0.5f;                            // Seconds to wait before spawning a new letter
-    private int lettersToWin;                                   // Number of letters to complete to win
+
 
     // Start on menu screen, and find the Player object
     void Start()
     {
         player = GameObject.Find("Player").GetComponent<PlayerTrail>();
+        menuUIController = GameObject.Find("Canvas").GetComponent<MenuUIController>();
     }
 
     // Check each frame whether any orbs remain; if not, spawn a new letter
@@ -40,7 +41,7 @@ public class GameManager : MonoBehaviour
 
             if (lettersComplete == lettersToWin)
             {
-                VictoryScreen();
+                menuUIController.VictoryScreen();
                 Invoke("WinGame", 1.0f);                      // End game if the player has completed enough letters
             } else {
                 Invoke("SpawnLetter", spawnDelay);            // Spawn a new letter after a one second delay
@@ -51,12 +52,8 @@ public class GameManager : MonoBehaviour
     public void StartGame()
     {
         isGameActive = true;                                                    // Activate the game
-        orbsRemainingText.gameObject.SetActive(true);                           // Show orbs remaining text
         lettersComplete = 0;                                                    // Set completed letters to zero
-        lettersCompleteText.text = "Letters Complete: " + lettersComplete;      // Set initial letters complete text
-        lettersCompleteText.gameObject.SetActive(true);                         // Show letters complete text
-        titleScreen.gameObject.SetActive(false);                                // Hide title screen text
-
+        menuUIController.ToGame();                                              // Set UI to game screen
         WinCondition();                                                         // Set the win condition
         SpawnLetter();                                                          // Spawn letter to start the game
     }
@@ -68,16 +65,13 @@ public class GameManager : MonoBehaviour
         selectedLetter = null;                              // Unload the selected letter
         player.ClearTrail();                                // Remove the last set of painted trails
         altGameMode = null;                                 // Reset altGameMode
-        orbsRemainingText.gameObject.SetActive(false);      // Hide orbs remaining text
-        lettersCompleteText.gameObject.SetActive(false);    // Hide letters complete text
-        winText.gameObject.SetActive(false);                // Hide win screen text
-        titleScreen.gameObject.SetActive(true);             // Show title screen text
+        menuUIController.ToMenu();                          // Set UI to menu screen
     }
 
     void IncreaseScore(int AddToScore)
     {
         lettersComplete += AddToScore;
-        lettersCompleteText.text = "Letters Complete: " + lettersComplete;
+        menuUIController.lettersCompleteText.text = "Letters complete: " + lettersComplete;
     }
 
     // Set number of letters needed to win
@@ -95,7 +89,6 @@ public class GameManager : MonoBehaviour
         {
             lettersToWin = 3;
         }
-
     }
 
     void SpawnLetter()
@@ -139,13 +132,7 @@ public class GameManager : MonoBehaviour
             orbsRemaining++;
         }
 
-        orbsRemainingText.text = "Orbs remaining: " + orbsRemaining;        // Update score text
-        player.destroyNext = 0;                                             // Reset the player's destroyNext variable
-    }
-
-    void VictoryScreen()
-    {
-        // Show the victory screen, then return to main menu after a delay
-        winText.gameObject.SetActive(true);
+        menuUIController.orbsRemainingText.text = "Orbs remaining: " + orbsRemaining;        // Update score text
+        player.destroyNext = 0;                                                             // Reset the player's destroyNext variable
     }
 }
---
+++ Assets/Scripts/MenuUIController.cs
@@ -0,0 +1,84 @@
+using TMPro;
+using UnityEditor;
+using UnityEngine;
+
+public class MenuUIController : MonoBehaviour
+{
+    // Variables for Menu and Instructions screens
+    public GameObject titleScreen;         // Game Object representing the title screen
+    public GameObject infoScreen;          // Game Object representing the instruction screen
+
+    // Variables for in-game HUD
+    public TextMeshProUGUI orbsRemainingText;       // Text displaying the remaining orbs during gameplay
+    public TextMeshProUGUI lettersCompleteText;     // Text displaying the number of completed letters during gameplay
+
+    // Variables for upper/lower case slider
+    private bool letterStyle = false;       // Start with lower case letters
+    public GameObject upperCase;            // Upper case buttons
+    public GameObject lowerCase;            // Lower case buttons
+
+    // Other
+    public GameManager gameManager;         // Game Manager
+    public TextMeshProUGUI winText;         // Text displaying when the player wins a round
+
+    void Start()
+    {
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+    }
+
+    // Toggle between Menu, Instructions, and Game UI
+    public void ToInstructions()
+    {
+        titleScreen.gameObject.SetActive(false);
+        infoScreen.gameObject.SetActive(true);
+    }
+
+    public void ToMenu()
+    {
+        titleScreen.gameObject.SetActive(true);             // Show title screen
+        infoScreen.gameObject.SetActive(false);             // Hide instruction screen
+        orbsRemainingText.gameObject.SetActive(false);      // Hide orbs remaining text
+        lettersCompleteText.gameObject.SetActive(false);    // Hide letters complete text
+        winText.gameObject.SetActive(false);                // Hide win screen text
+    }
+
+    public void ToGame()
+    {
+        orbsRemainingText.gameObject.SetActive(true);                                   // Show orbs remaining text
+        lettersCompleteText.text = "Letters complete: " + gameManager.lettersComplete;  // Set initial letters complete text
+        lettersCompleteText.gameObject.SetActive(true);                                 // Show letters complete text
+        titleScreen.gameObject.SetActive(false);                                        // Hide title screen text
+    }
+
+    // Switch between upper- and lower-case letters in the menu based on a toggle
+    public void SwitchLetterStyle()
+    {
+        letterStyle = !letterStyle;
+
+        if (letterStyle)
+        {
+            upperCase.gameObject.SetActive(true);
+            lowerCase.gameObject.SetActive(false);
+        }
+        else if (!letterStyle)
+        {
+            upperCase.gameObject.SetActive(false);
+            lowerCase.gameObject.SetActive(true);
+        }
+    }
+
+    // Show the victory screen, then return to main menu after a delay
+    public void VictoryScreen()
+    {
+        winText.gameObject.SetActive(true);
+    }
+
+    public void ExitGame()
+    {
+    #if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+    #else
+	    Application.Quit();
+    #endif
+    }
+}
